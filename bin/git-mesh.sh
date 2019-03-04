ab0558f1b004aecd4534ec7c0e141794a47bebf2
TsT <tst2005@gmail.com>
2019-03-04 14:43:07 +0100

    quick saving

--- bin/git-mesh.sh
+++ bin/git-mesh.sh
@@ -8,21 +8,29 @@ stop() {
 	exit ${2:-1}
 }
 
-isenabled() {
+subcmd() {
+	local cmd="$1$2";shift 2
+	"$cmd" "$@"
+}
+
+git_mesh_config_status() {
 	local enabled=$(git config --local --bool --get mesh.enabled)
 	printf %s\\n "${enabled:-false}"
 }
-enable() {
+git_mesh_config_enable() {
 	git config --local mesh.enabled true;
 }
-disable() {
+git_mesh_config_disable() {
 	git config --local mesh.enabled false;
 }
+git_mesh_config_forget() {
+	git config --local --unset mesh.enabled;
+}
 
-getpeers() {
+git_mesh_config_getpeers() {
 	git config --local --get mesh.peers;
 }
-setpeers() {
+git_mesh_config_setpeers() {
 	if [ -z "$1" ]; then
 		git config --local --unset mesh.peers
 	else
@@ -30,27 +38,15 @@ setpeers() {
 	fi
 }
 
-config() {
+git_mesh_config() {
 	case "$1" in
-		(status)
-			isenabled
-		;;
-		(enable)
-			shift; enable
-		;;
-		(disable)
-			shift; disable
-		;;
-		(setpeers)
-			shift; setpeers "$@"
-		;;
-		(getpeers)
-			shift; getpeers
+		(status|enable|disable|forget|getpeers|setpeers)
+			subcmd git_mesh_config_ "$@"
 		;;
 	esac
 }
 
-setup() {
+git_mesh_setup() {
 	for peer in $MESH_PEERS; do
 		[ "$peer" != "$me" ] || continue 
 		if ! git remote | grep -q "$peer"; then
@@ -58,7 +54,7 @@ setup() {
 		fi
 	done
 }
-peers() {
+git_mesh_peers() {
 	[ $# -gt 0 ] || set -- "list";
 	case "$1" in
 		(list)
@@ -75,7 +71,7 @@ peers() {
 		;;
 	esac
 }
-peer() {
+git_mesh_peer() {
 	[ $# -gt 0 ] || set -- "help";
 	case "$1" in
 		(help)		stop "Usage: git mesh peer [add|remove|help]" 0 ;;
@@ -85,49 +81,54 @@ peer() {
 	esac
 }
 
-fetch_all() {
+git_mesh_fetch_all() {
 	for peer in $(peers); do
-		fetch $peer
+		subcmd git_mesh_ fetch $peer
 	done
 }
-fetch() {
+git_mesh_fetch() {
 	git fetch "$@"
 }
-rfetch() {
+git_mesh_rfetch() {
 	local target="$1";shift;
 	ssh -n "$target" 'cd '"$(pwd)"' && ./git-mesh.sh fetch '"$me"
 }
-update_master() {
-	: git co master && git rebase foo/master && git co $localme
+#git_mesh_update_master() {
+#	: git co master && git rebase foo/master && git co $localme
+#}
+
+
+git_mesh() {
+	MESH_ENABLED="$(git config --local --bool --get mesh.enabled)"
+	MESH_ENABLED="${MESH_ENABLED:-false}"
+
+	#MESH_PEERS="$(git config --get mesh.peers)"
+
+	#[ -n "$MESH_PEERS" ] || stop "ERROR: MESH_PEERS undefined" 1
+
+	#me="$MESH_ME"
+	#[ -n "$me" ] || me=$(uname -n)
+	#echo "# me=$me"
+	echo "# MESH_ENABLED=$MESH_ENABLED"
+	echo "# MESH_PEERS=$MESH_PEERS"
+
+	if ! $MESH_ENABLED; then
+		case "$1" in
+			(config) ;;
+			(help) ;;
+			(*) stop "MESH is not enabled" ;;
+		esac
+	fi
+
+	case "$1" in
+		(config)	subcmd git_mesh_ "$@" ;;
+		(setup)		subcmd git_mesh_ "$@" ;;
+		(fetch)		subcmd git_mesh_ "$@" ;;
+		(fetch_all)	subcmd git_mesh_ "$@" ;;
+		(rfetch)	subcmd git_mesh_ "$@" ;;
+		(peers)		subcmd git_mesh_ "$@" ;;
+		(*) echo "USAGE";exit 1;;
+	esac
 }
 
-MESH_ENABLED="$(git config --local --bool --get mesh.enabled)"
-MESH_ENABLED="${MESH_ENABLED:-false}"
-
-#MESH_PEERS="$(git config --get mesh.peers)"
-
-#[ -n "$MESH_PEERS" ] || stop "ERROR: MESH_PEERS undefined" 1
-
-#me="$MESH_ME"
-#[ -n "$me" ] || me=$(uname -n)
-#echo "# me=$me"
-echo "# MESH_ENABLED=$MESH_ENABLED"
-echo "# MESH_PEERS=$MESH_PEERS"
-
-if ! $MESH_ENABLED; then
-	case "$1" in
-		(config) ;;
-		(help) ;;
-		(*) stop "MESH is not enabled" ;;
-	esac
-fi
-
-case "$1" in
-	(config) shift; config "$@";;
-	(setup) setup;;
-	(fetch) shift; fetch "$@";;
-	(fetch_all) fetch_all;;
-	(rfetch) shift; rfetch "$@";;
-	(peers) peers;;
-	(*) echo "USAGE";exit 1;;
-esac
+git_mesh "$@"
